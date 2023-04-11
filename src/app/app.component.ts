faad3c4662c8536fb4d9e533988b451caa86792d
sateesh gedela <sateeshgedela4219@gmail.com>
2023-04-11 12:21:15 +0530

    change all comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ViewChild, } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { CoreService } from './core/core.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular1';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+     'firstName',
+     'lastName',
+     'email',
+     'dob',
+     'gender',
+     'education',
+     'company',
+     'experience',
+     'package',
+     'action',
+    
+    ];
+    dataSource!: MatTableDataSource<any>;
+  
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+    title = 'angular1';
+    constructor(private dialog:MatDialog, private empService:EmployeeService,
+      private coreservice:CoreService){
+  
+    }
+  ngOnInit(): void {
+    this.getEmployeeList();
+    
+  }
+  OpenAddEditEmpForm(){
+    const dialogRef =  this.dialog.open(EmpAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next:(val)=>{
+        if (val){
+          this.getEmployeeList();
+        }
+      },
+    });
+    }
+    getEmployeeList(){
+      this.empService.getEmployeeList().subscribe({
+        next:(res)=>{
+          this.dataSource=new MatTableDataSource(res);
+          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.paginator;
+  
+        },
+        error:(err)=>{
+          console.log(err);
+        },
+      });
+    }
+    applyFilter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+  
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
+    }
+    deleteEmployee(id:number){
+      this.empService.deleteEmployee(id).subscribe({
+        next:(res)=>{
+          alert('Employee deleted!');
+          this.coreservice.openSnackBar('Employee deleted!','done');
+          this.getEmployeeList();
+  
+        },
+        error:(err)=>{
+          console.log(err);
+        },
+      });
+  
+    }
+    openEditForm(data:any){
+      const dialogRef =  this.dialog.open(EmpAddEditComponent,{
+          data:data
+          
+        });
+        dialogRef.afterClosed().subscribe({
+          next:(val)=>{
+            if (val){
+              this.getEmployeeList();
+            }
+          },
+        });
+  
+     
+      }
+  
 }
---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './emp-add-edit.component.html',
+  styleUrls: ['./emp-add-edit.component.css']
+})
+export class EmpAddEditComponent {
+education: any;
+data: any;
+onFormSubmit() {
+throw new Error('Method not implemented.');
+}
+empForm: FormGroup<any>;
+
+}
